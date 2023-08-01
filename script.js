74025da9d4e53284ef1ecab30dc5b0c48de1256d
Advaitmenon1106 <advaitmenon1106@gmail.com>
2023-08-01 23:09:05 +0530

    Home screen built (not connected to the easy mode html). EasyMode.html (game at easy difficulty) has been initiated as a project folder and the HTML-CSS is designed for the same.

---
+++ script.js
@@ -0,0 +1,20 @@
+let diff = document.getElementsByClassName('difficulty-levels');
+
+for (let i = 0; i<diff.length; i++){
+    diff[i].addEventListener('mouseover', ()=>{
+        diff[i].style.boxShadow = '1px 7px 1px 7px rgb(230, 230, 230)';
+        diff[i].style.color = 'rgb(0, 0, 0)'
+    })
+    diff[i].addEventListener('mouseout', ()=>{
+        diff[i].style.boxShadow = '1px 5px 1px 5px rgb(230, 230, 230)';
+        diff[i].style.color = 'rgb(75, 75, 75)'
+    })
+    diff[i].addEventListener('mousedown', ()=>{
+        diff[i].style.boxShadow = '0px 1px 0px 1px rgb(230, 230, 230)';
+        diff[i].style.fontSize = '45px';
+        diff[i].addEventListener('mouseup', ()=>{
+            diff[i].style.boxShadow = '1px 7px 1px 7px rgb(230, 230, 230)';
+            diff[i].style.fontSize = '50px';
+        })
+    })
+}
