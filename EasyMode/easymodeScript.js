d295afb3858a8e500212c47434f3dd8028118071
Advaitmenon1106 <advaitmenon1106@gmail.com>
2023-08-03 11:27:21 +0530

    Finished with easy mode.

--- EasyMode/easymodeScript.js
+++ EasyMode/easymodeScript.js
@@ -1,9 +1,8 @@
 "use strict";
 
-let numberToGuess = Math.random()*500;
+let numberToGuess = Math.floor(Math.random()*300);
 
 const checkGuess = ()=>{
-    console.log(counter);
     let guessedNumber = guessInput.value;
     let warningToast = document.getElementById('toast');
     let warningMessage = document.getElementById('message');
@@ -19,10 +18,10 @@ const checkGuess = ()=>{
     font-size: 25px;
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
-    padding: 5%
-    `
+    padding: 5%;
+    `;
 
-    guessTimesBand.innerHTML = 'Hint x'
+    guessTimesBand.innerHTML = 'Hint '+ (counter+1);
 
     guessHistoryUnit.style.cssText = `border:2px solid whitesmoke;
     border-radius: 15px;`;
@@ -40,26 +39,56 @@ const checkGuess = ()=>{
     box-shadow: 1px 5px 1px 5px rgb(230, 230, 230);
     `;
 
-    if (guessedNumber.length ==0){
+    if (guessedNumber.length == 0){
         warningMessage.innerHTML = 'Enter a valid number!';
         warningToast.className +=' show';
         setTimeout(() => {
             warningToast.className = warningToast.className.replace(' show', '');
         }, 3000);
     }
-    else if (counter==6){
-        warningMessage.innerHTML = 'Exceeded 6 hints. Game over!'
+
+    else if (guessedNumber == numberToGuess){
+        document.getElementsByClassName('toastMsgBand')[0].innerHTML = 'Congratulations!'
+        warningMessage.innerHTML = 'You\'ve guessed it correctly!';
         warningToast.className +=' show';
         setTimeout(() => {
             warningToast.className = warningToast.className.replace(' show', '');
         }, 3000);
     }
+
+    else if (counter==9){
+        warningMessage.innerHTML = 'Exceeded 6 hints. Game over!'
+        warningToast.className +=' show';
+        setTimeout(() => {
+            warningToast.className = warningToast.className.replace(' show', '');
+        }, 3000);
+        guessedNumber = null;
+        
+    }
+
     else{
         guessHistoryUnit.appendChild(guessTimesBand);
         guessingGrid.appendChild(guessHistoryUnit);
         guessHistoryUnit.appendChild(historicalGuessDisplay);
         historicalGuessDisplay.innerHTML = guessedNumber;
         counter = counter+1;
+        let hintDiv = document.createElement('div');
+        hintDiv.style.fontSize = '30px';
+        hintDiv.style.fontFamily = "'Comic Neue', serif";
+        hintDiv.style.marginTop = '10%';
+
+
+        if (guessedNumber>numberToGuess){
+            hintDiv.innerHTML = 'The actual code is lesser than your guess.';
+            guessHistoryUnit.appendChild(hintDiv);
+        }
+
+
+        else if (guessedNumber<numberToGuess){
+            hintDiv.innerHTML = 'The actual code is higher than your guess.';
+            guessHistoryUnit.appendChild(hintDiv);
+        }
+        guessInput.value='';
     }
 }
 
@@ -68,5 +97,6 @@ let counter = 0;
 let guessingGrid = document.getElementById('guessingGrid');
 guessingGrid.style.marginTop = '5%';
 let guessInput = document.getElementById('input');
+checkButton.style.cursor = 'pointer';
 
 checkButton.addEventListener('click', checkGuess);
